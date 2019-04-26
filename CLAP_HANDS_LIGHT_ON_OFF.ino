a8c28ec051da4c704bfdbf8ad2153767ef680806
Projecto <50033436+ProjectoOfficial@users.noreply.github.com>
2019-04-27 00:45:01 +0200

    Add files via upload

---
+++ CLAP_HANDS_LIGHT_ON_OFF.ino
@@ -0,0 +1,32 @@
+#define del 500
+#define mic A0
+#define add 10
+int soglia = 65;
+int cont = 0;
+int azzera = 200;
+bool stato=0;
+
+void setup() {
+  pinMode(mic, INPUT);
+  pinMode(add, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(analogRead(mic));
+  if (analogRead(mic) > soglia) {
+    cont++;
+    delay(del);
+  }
+    if (azzera == 0) {
+      azzera = 100;
+      cont = 0;
+    }
+    if (cont == 2) {
+      azzera = 100;
+      digitalWrite(add, !digitalRead(add));
+      stato=!stato;
+      cont = 0;
+    }
+  azzera--;
+}
